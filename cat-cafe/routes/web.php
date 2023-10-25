15022b0e9f7eba48ccc9e961553bdc183e814404
test54678 <eisaku546@gmail.com>
2023-10-25 18:01:07 +0900

    バリデーションの項目

--- cat-cafe/routes/web.php
+++ cat-cafe/routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ContactController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/contact',[ContactController::class, 'index'])->name('contact');
+Route::post('/contact',[ContactController::class, 'sendMail']);
+Route::get('/contact/complete',[ContactController::class, 'complete'])->name('contact.complete');
