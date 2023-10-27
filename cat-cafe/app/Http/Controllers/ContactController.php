8734e8becc5587d9a2cf1d8a763fd7e5230e7d96
test54678 <eisaku546@gmail.com>
2023-10-27 17:21:05 +0900

    バリデーション　他

--- cat-cafe/app/Http/Controllers/ContactController.php
+++ cat-cafe/app/Http/Controllers/ContactController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ContactRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
@@ -14,15 +15,9 @@ class ContactController extends Controller
         return view('contact.index');
     }
 
-    function sendMail(Request $request) {
+    function sendMail(ContactRequest $request) {
         session(['abcdefghijk' => 9]);
-        $validated = $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'name_kana' => ['required', 'string', 'max:255', 'regex:/^[ァ-ロワンヴー]*$/u'],
-            'phone' => ['nullable', 'regex:/^0(\d-?\d{4}|\d{2}-?\d{3}|\d{3}-?\d{2}|\d{4}-?\d|\d0-?\d{4})-?\d{4}$/'],
-            'email' => ['required', 'email'],
-            'body' => ['required', 'string', 'max:2000'],
-        ]);
+        $validated = $request->validated();
     
         // これ以降の行は入力エラーがなかった場合のみ実行されます
         // 登録処理(実際はメール送信などを行う)
