78b9af8d5616d141cd3c1eb3612b0850a204bee9
test54678 <eisaku546@gmail.com>
2023-11-12 19:09:25 +0900

    治った

--- cat-cafe/app/helpers.php
+++ cat-cafe/app/helpers.php
@@ -3,19 +3,26 @@
 
 if (! function_exists('custom_function')) {
 
-    function custom_function($now_date,$i,$yyy,$zzz,$xxx)
+    // function custom_function($now_date,$i,$yyy,$zzz,$xxx)
+    function custom_function($now_date_hizuke,$i,$yyy,$zzz,$xxx)
     {
         // $z = $x + $y;
         // return $z; 
         // 実装内容
+        $day = date('d', strtotime($i));
+        $day = ltrim($day, '0');
+
         $week2 ="";
-        if ($now_date == $i) {
+        if ($now_date_hizuke == $i) {
+            // if ($now_date == $i) {
             // $week2 .= '<div class="xyz today_mark">' . $i . '</div>';
             $week2 .= '<div class="number-circle">';
-            $week2 .= '<span class="number">' . $i . '</span>';
+            $week2 .= '<span class="number">' . $day . '</span>';
+            // $week2 .= '<span class="number">' . $i . '</span>';
             $week2 .= '</div>';
         } else {
-            $week2 .= '<div class="xyz ">' . $i . '</div>';
+            $week2 .= '<div class="xyz ">' . $day . '</div>';
+            // $week2 .= '<div class="xyz ">' . $i . '</div>';
         }
         $week2 .= '<div class="yyy">' . $yyy . '</div>';
         $week2 .= '<div class="zzz">' . $zzz . '</div>';
